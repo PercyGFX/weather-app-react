2d18f6a66c9662a656d2042be72991b3f77bdc3a
PercyGFX <isurangabtk@gmail.com>
2023-06-06 16:44:18 +0530

    talewind install + reactunicorn icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1 className="font-bold">Hello</h1>
+        <UilReact />
     </div>
   );
 }
